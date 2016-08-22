d570ae1abe158764b1bd13d24142cd7a7e9b78a6
phou <pygnasak@gmail.com>
2016-08-22 11:03:38 -0500

    Revert Demo

--- HTML-Forms/Thanks.aspx.cs
+++ HTML-Forms/Thanks.aspx.cs
@@ -11,7 +11,7 @@ namespace HTML_Forms
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            //== Adding changes for demonstration purposes
         }
     }
 }
